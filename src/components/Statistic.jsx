a9102d36f78929b3abc88f5b746914ca41cc9f94
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-01-01 14:33:49 +0530

    submit

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -6,48 +6,150 @@ import {
     StatArrow,
     StatGroup,
     Box,
+    Heading,
 } from '@chakra-ui/react'
+import { useState } from 'react'
+
+import CountUp from 'react-countup'
+import ScrollTrigger from 'react-scroll-trigger'
 
 
 export default function Statistic() {
+    const [state, setState] = useState(false);
     return (
         <>
-        <Box padding={{base:'1rem', md: '4rem'}} bg={'#ddd'} color={'blueviolet'}>
+            <Box padding={{ base: '1rem', md: '4rem' }} bg={'#ddd'} color={'blueviolet'}>
 
-            <StatGroup gap={'1rem'}>
-                <Stat minW={'120px'}>
-                    <StatLabel>Lorem Ipsum</StatLabel>
-                    <StatNumber fontSize={'2rem'}>123+</StatNumber>
-                    <StatHelpText>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque 
-                    </StatHelpText>
-                </Stat>
-                <Stat minW={'120px'}>
-                    <StatLabel>Lorem Ipsum</StatLabel>
-                    <StatNumber fontSize={'2rem'}>12+</StatNumber>
-                    <StatHelpText>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque ipsum porro,
-                    </StatHelpText>
-                </Stat>
-              
-                <Stat minW={'120px'}>
-                    <StatLabel>Lorem Ipsum</StatLabel>
-                    <StatNumber fontSize={'2rem'}>12+</StatNumber>
-                    <StatHelpText>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque ipsum
-                    </StatHelpText>
-                </Stat>
-                <Stat minW={'120px'}>
-                    <StatLabel>Lorem Ipsum</StatLabel>
-                    <StatNumber fontSize={'2rem'}>123</StatNumber>
-                    <StatHelpText>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque
-                    </StatHelpText>
-                </Stat>
+                <StatGroup gap={'1rem'}>
+                    <Stat minW={'120px'}>
+                        <StatLabel>Lorem Ipsum</StatLabel>
+                        <StatNumber fontSize={'2rem'}>
+
+                            {/* this */}
+
+                            <ScrollTrigger onEnter={() => setState(true)}>
+
+                                <Heading>
+
+                                    {
+                                        state ?
+
+                                            <CountUp start={0} end={123}
+                                                duration={3}
+
+                                            /> : '0'
+                                    }+
+                                </Heading>
+
+                            </ScrollTrigger>
 
 
-            </StatGroup>
-        </Box>
+
+                            {/*this  */}
+
+                        </StatNumber>
+                        <StatHelpText>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque
+                        </StatHelpText>
+                    </Stat>
+                    <Stat minW={'120px'}>
+                        <StatLabel>Lorem Ipsum</StatLabel>
+                        <StatNumber fontSize={'2rem'}>
+  {/* this */}
+
+  <ScrollTrigger onEnter={() => setState(true)}>
+
+<Heading>
+
+    {
+        state ?
+
+            <CountUp start={0} end={12}
+                duration={3}
+
+            /> : '0'
+    }+
+</Heading>
+
+</ScrollTrigger>
+
+
+
+{/*this  */}
+
+
+                        </StatNumber>
+                        <StatHelpText>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque ipsum porro,
+                        </StatHelpText>
+                    </Stat>
+
+                    <Stat minW={'120px'}>
+                        <StatLabel>Lorem Ipsum</StatLabel>
+                        <StatNumber fontSize={'2rem'}>
+
+  {/* this */}
+
+  <ScrollTrigger onEnter={() => setState(true)}>
+
+<Heading>
+
+    {
+        state ?
+
+            <CountUp start={0} end={12}
+                duration={3}
+
+            /> : '0'
+    }+
+</Heading>
+
+</ScrollTrigger>
+
+
+
+{/*this  */}
+
+                        </StatNumber>
+                        <StatHelpText>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque ipsum
+                        </StatHelpText>
+                    </Stat>
+                    <Stat minW={'120px'}>
+                        <StatLabel>Lorem Ipsum</StatLabel>
+                        <StatNumber fontSize={'2rem'}>
+
+  {/* this */}
+
+  <ScrollTrigger onEnter={() => setState(true)}>
+
+<Heading>
+
+    {
+        state ?
+
+            <CountUp start={0} end={123}
+                duration={3}
+
+            /> : '0'
+    }
+</Heading>
+
+</ScrollTrigger>
+
+
+
+{/*this  */}
+
+                        </StatNumber>
+                        <StatHelpText>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum beatae explicabo rerum minus optio quasi officiis voluptatum. Vitae eaque
+                        </StatHelpText>
+                    </Stat>
+
+
+                </StatGroup>
+            </Box>
         </>
     )
 }
